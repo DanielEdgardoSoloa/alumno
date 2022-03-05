241f571bb412495550412b557e40fffa115c1840
DanielSoloa <danieledgardosoloa@gmail.com>
2022-03-05 19:45:09 -0300

    Se agrega aside de noticias a inicio.php

--- index.php
+++ index.php
@@ -4,6 +4,31 @@
     <head>
         <meta charset="UTF-8">
         <title>Inicio</title>
+        <style>
+            .container{
+                background-color: green;
+                background-image: url(instituto1.jpg);
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+                height: 550px;
+            }
+            .menu{
+                background-color: #6c757d;
+                width: 200px;
+                height: 450px;
+                float: left;
+            }
+            .noticias{
+                position: relative ;
+                background-color: #6c757d;
+                width: 200px;
+                height: 450px;
+                margin-left: 713px;
+                float: left;
+            }
+        </style>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Latest compiled and minified CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -13,8 +38,9 @@
     </head>
     <body class="bg-secondary">
         <div class="container">
-          <h1 clsss="text-success">LEGAJOS ALUMNOS</h1> 
-          <div class="datosPersonales">
+          <h1 class="text-dark">LEGAJOS ALUMNOS</h1> 
+          <div class="menu">
+              <h3 class="text-dark">MENU</h3>
             <form action="tablaAlumnos.php" method="POST">
               <br/><br/>
               <input type="submit" name="Enviar" class="btn btn-primary" value="Ir a Alumnos" class="enviar" >
@@ -31,6 +57,16 @@
               <br/><br/>
               <input type="submit" name="Enviar" class="btn btn-primary" value="Ir a Notas" class="enviar" >
             </form>  
+          </div>
+          <div class="noticias" id='posicion3'>
+             <h3 class="text-dark">NOTICIAS</h3>
+             <ul>
+              <li> Mesas Feb-Mar 2021</li>
+              <li> Consultas Feb-Mar 2021</li>
+              <li> Solicitud equivalencias 2022</li>
+              <li> Ingreso a Indumentaria: AVISO</li>
+              <li> Ingreso Computación y Redes 2022</li>
+          </ul>
           </div>
         </div>
     </body>
